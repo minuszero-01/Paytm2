be2f008e3dd0ab19a7949e5ce5d6e9bb4b138318
minuszero-01 <harshhande95@gmail.com>
2024-07-11 12:28:02 +0530

    Done

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,14 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "./lib/auth";
+
+export default async function Page() {
+  console.log("here");
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  if (session?.user) {
+    redirect("/dashboard");
+  } else {
+    redirect("/api/auth/signin");
+  }
+}
